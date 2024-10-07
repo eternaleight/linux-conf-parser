4a3c0ca138a750b3af79b4c4e5494a1f98dd9f19
eternaleight <eternaleight0@gmail.com>
2024-10-08 02:04:41 +0900

    fix

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,15 +1,34 @@
-use std::io;
+use rand::Rng;
+use std::{cmp::Ordering, io};
 
 fn main() {
     println!("Guess the number!");
 
+    let secret_number: i32 = rand::thread_rng().gen_range(1..101);
+
+    println!("The secret numner is: {}", secret_number);
+
     println!("Please input your guess.");
 
-    let mut guess = String::new();
+    let mut guess: String = String::new();
 
     io::stdin()
         .read_line(&mut guess)
         .expect("Failed to your guess.");
 
+    // 入力が空でないかを確認
+    if guess.trim().is_empty() {
+        println!("You didn't enter a guess!");
+        return; // プログラムを終了する
+    }
+
+    let guess: i32 = guess.trim().parse().expect("Please type a number!");
+
     println!("You guessed: {}", guess);
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small"),
+        Ordering::Greater => println!("Too big"),
+        Ordering::Equal => println!("You win"),
+    }
 }
